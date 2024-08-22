e10332a3b6f3fd990211331a22c5bd64496ff144
Truong Quoc Vuong <vanvuong10801@gmail.com>
2024-08-22 15:31:55 +0700

    bai2

--- demo.js
+++ demo.js
@@ -1,3 +1,9 @@
-function change_color() {
-    document.getElementById("text").classList.toggle("myStyle");
-}
+const button = document.getElementById("Btn");
+const text = document.getElementById("text");
+button.addEventListener("click",function(){
+    text.style.display = "block";
+
+    setTimeout(function() {
+    text.style.display = "none";
+},5000);
+});
