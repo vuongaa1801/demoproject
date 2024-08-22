a432714e806ec44b39187f55962280fa339053ec
Truong Quoc Vuong <vanvuong10801@gmail.com>
2024-08-22 14:30:56 +0700

    sua theo Dom

--- demo.js
+++ demo.js
@@ -1,8 +1,3 @@
-document.getElementById('ChangeColor').onclick = function(){
-    var text = document.getElementById('text');
-    if(text.style.color === 'black'){
-        text.style.color = 'red';
-    }else {
-        text.style.color = 'black';
-    }
+function change_color() {
+    document.getElementById("text").classList.toggle("myStyle");
 }
